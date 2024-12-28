f78c0bc3a971e4206060a9099b89ee10d5683b33
Francisvan Oliveira <francisvan.oliveira@francisvanoliveira.com.br>
2024-12-27 23:27:59 -0400

    Modal e Option

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,10 +1,11 @@
-import { View, Image, TouchableOpacity, FlatList } from "react-native"
+import { View, Image, TouchableOpacity, FlatList, Modal, Text } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
 
 import { styles } from "./styles"
 import { colors } from "@/styles/colors"
 
 import { Link } from "@/components/link"
+import { Option } from "@/components/option"
 import { Categories } from "@/components/categories"
 
 export default function Index(){
@@ -35,6 +36,32 @@ export default function Index(){
                 showsVerticalScrollIndicator={false}
 
             />
+
+            <Modal transparent visible={true}>
+                <View style={styles.modal}>
+                    <View style={styles.modalContent}>
+                        <View style={styles.modalHeader}>
+                            <Text style={styles.modalCategory}>Curso</Text>
+                            <TouchableOpacity>
+                                <MaterialIcons name="close" size={20} color={colors.gray[400]} />
+                            </TouchableOpacity>
+                        </View>
+
+                        <Text style={styles.modalLinkName}>
+                            Francisvan
+                        </Text>
+
+                        <Text style={styles.modalUrl}>
+                            https://francisvanoliveira.com.br
+                        </Text>
+
+                        <View style={styles.modalFooter}>
+                            <Option name="Excluir" icon="delete" variant="secondary"/>
+                            <Option name="Abrir" icon="language"/>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
         </View>
     )
 }
